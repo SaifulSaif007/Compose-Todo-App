610282d7b1bc0baa85dafb9687c74e2d36445f79
Saiful Islam <saifulsaif321@gmail.com>
2023-02-10 21:54:51 +0600

    task ui

--- app/src/main/java/com/saiful/composetodoapp/ui/screen/TaskScreen.kt
+++ app/src/main/java/com/saiful/composetodoapp/ui/screen/TaskScreen.kt
@@ -1,12 +1,72 @@
 package com.saiful.composetodoapp.ui.screen
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
+import com.saiful.composetodoapp.TodoListScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        topBar = {
+            SmallTopAppBar(
+                title = { Text(text = "Add new task") },
+            )
+        },
+    ) { contentPadding ->
+        Box(modifier = Modifier.padding(contentPadding)) {
+            Surface {
+                TaskScreenContent()
+            }
+        }
+    }
+}
 
+@Composable
+fun TaskScreenContent() {
+    var taskName by remember { mutableStateOf("") }
+    var taskDescription by remember { mutableStateOf("") }
+
+    Column {
+        TextField(
+            value = taskName,
+            onValueChange = { taskName = it },
+            label = { Text(text = "Task Name") },
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 2.dp)
+                .fillMaxWidth()
+        )
+
+        TextField(
+            value = taskDescription,
+            onValueChange = { taskDescription = it },
+            label = { Text(text = "Task Description") },
+            modifier = Modifier
+                .padding(horizontal = 16.dp, vertical = 12.dp)
+                .fillMaxWidth()
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Button(
+                modifier = Modifier
+                    .padding(horizontal = 12.dp)
+                    .widthIn(100.dp, 400.dp),
+                onClick = { /*TODO*/ }) {
+                Text(text = "Add")
+            }
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
